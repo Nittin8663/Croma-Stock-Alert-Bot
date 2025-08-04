01d2628f740c174aa5944da8503443a8498f57c0
Nittin8663 <90694841+Nittin8663@users.noreply.github.com>
2025-08-05 00:39:15 +0530

    Update test_chrome.py

--- test_chrome.py
+++ test_chrome.py
@@ -3,11 +3,11 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.binary_location = "/usr/bin/google-chrome"
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(options=options)
-driver.get('https://www.google.com')
+driver.get("https://www.google.com")
 print(driver.title)
 driver.quit()
