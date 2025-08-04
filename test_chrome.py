e84e24a9d9de7fdc29a1a13c2e010f7eddd2506e
Nittin8663 <90694841+Nittin8663@users.noreply.github.com>
2025-08-05 00:56:05 +0530

    Update test_chrome.py

--- test_chrome.py
+++ test_chrome.py
@@ -1,6 +1,7 @@
+import tempfile
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import tempfile
 
 options = Options()
 options.binary_location = "/usr/bin/google-chrome"
@@ -8,8 +9,7 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-# Create a unique temp directory for user data
-with tempfile.TemporaryDirectory() as user_data_dir:
+with tempfile.TemporaryDirectory(dir=os.path.expanduser("~")) as user_data_dir:
     options.add_argument(f"--user-data-dir={user_data_dir}")
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.google.com")
