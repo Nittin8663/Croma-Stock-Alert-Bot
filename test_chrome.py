0c8e30aacbcbf82067903f9a83fd1817b67d4984
Nittin8663 <90694841+Nittin8663@users.noreply.github.com>
2025-08-05 00:53:21 +0530

    Update test_chrome.py

--- test_chrome.py
+++ test_chrome.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import tempfile
 
 options = Options()
-options.binary_location = "/usr/bin/google-chrome"  # Chrome symlink target
+options.binary_location = "/usr/bin/google-chrome"
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.google.com")
-print(driver.title)  # Should print "Google"
-driver.quit()
+with tempfile.TemporaryDirectory() as tmpdirname:
+    options.add_argument(f'--user-data-dir={tmpdirname}')
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.google.com")
+    print(driver.title)
+    driver.quit()
