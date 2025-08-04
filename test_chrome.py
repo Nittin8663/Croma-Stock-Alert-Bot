1d5fcbeab4d05c8d2c1865b38dccca9c7c047459
Nittin8663 <90694841+Nittin8663@users.noreply.github.com>
2025-08-05 00:52:08 +0530

    Update test_chrome.py

--- test_chrome.py
+++ test_chrome.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.binary_location = "/usr/bin/google-chrome"
+options.binary_location = "/usr/bin/google-chrome"  # Chrome symlink target
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
