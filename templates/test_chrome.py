caa42872deb7ce6b5e7d57499f924f1bbeb26bbf
Nittin8663 <90694841+Nittin8663@users.noreply.github.com>
2025-08-05 00:33:52 +0530

    Create test_chrome.py

---
+++ templates/test_chrome.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.binary_location = "/usr/bin/google-chrome"
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=options)
+driver.get('https://www.google.com')
+print(driver.title)
+driver.quit()
